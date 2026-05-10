87ba7af2fd13afb9a6c37042642eccc061188724
abdullah alresheed <abdullah122k@outlook.com>
2026-05-11 01:09:33 +0300

    Update ProductController.java

--- JPA-Exercise/src/main/java/com/example/jpaexercise/Controller/ProductController.java
+++ JPA-Exercise/src/main/java/com/example/jpaexercise/Controller/ProductController.java
@@ -6,6 +6,7 @@ import com.example.jpaexercise.Service.ProductService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
