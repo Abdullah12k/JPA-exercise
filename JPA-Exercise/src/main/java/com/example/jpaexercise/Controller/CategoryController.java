74398ea74429b55f751690f80c5cc63f82a2c22e
abdullah alresheed <abdullah122k@outlook.com>
2026-05-11 01:07:32 +0300

    Update CategoryController.java

--- JPA-Exercise/src/main/java/com/example/jpaexercise/Controller/CategoryController.java
+++ JPA-Exercise/src/main/java/com/example/jpaexercise/Controller/CategoryController.java
@@ -6,6 +6,7 @@ import com.example.jpaexercise.Service.CategoryService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
